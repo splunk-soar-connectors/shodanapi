7a350df231459e0045fce5b1c024716ea9cd587e
aparekh-crest <aparekh@splunk.com>
2023-09-04 12:58:01 +0530

    removed unused library

--- shodan_connector.py
+++ shodan_connector.py
@@ -13,8 +13,6 @@
 # either express or implied. See the License for the specific language governing permissions
 # and limitations under the License.
 
-from __future__ import print_function, unicode_literals
-
 import copy
 import ipaddress
 import json
